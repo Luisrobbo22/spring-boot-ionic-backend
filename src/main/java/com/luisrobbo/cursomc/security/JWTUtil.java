5730fb4143f27b9c04c81a27e61d1998f6825aa7
Luis Robbo <luis_robbo@hotmail.com>
2020-07-27 20:05:47 -0300

    Implementando Autorizacao

--- src/main/java/com/luisrobbo/cursomc/security/JWTUtil.java
+++ src/main/java/com/luisrobbo/cursomc/security/JWTUtil.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
@@ -23,6 +24,42 @@ public class JWTUtil {
 				.setExpiration(new Date(System.currentTimeMillis() + expiration))
 				.signWith(SignatureAlgorithm.HS512, secret.getBytes())
 				.compact();
+	}
+
+	public boolean tokenValido(String token) {
+
+		// Claims armazena a reivindicacao do token
+		// alega que é um usuario xpto e o tempo de expiracao x
+		Claims claims = getClaims(token);
+		if (claims != null) {
+			String username = claims.getSubject();
+			Date expirationDate = claims.getExpiration();
+			Date now = new Date(System.currentTimeMillis());
+
+			if (username != null && expirationDate != null && now.before(expirationDate)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public String getUsername(String token) {
+		Claims claims = getClaims(token);
+		if (claims != null) {
+			return claims.getSubject();
+		}
+		return null;
+	}
+
+	private Claims getClaims(String token) {
+
+		// recupera claims a partir do token
+		// pode ser nulo - tratar erro
+		try {
+			return Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(token).getBody();
+		} catch (Exception e) {
+			return null;
+		}
 	}
 
 }
