a2df8ea19ffb975be9f9485546d424c3546b0dc3
luisrobbo <Luisrobbo22>
2021-01-29 15:09:03 -0300

    Salvando URL da imagem em Cliente

--- src/main/java/com/luisrobbo/cursomc/CursomcApplication.java
+++ src/main/java/com/luisrobbo/cursomc/CursomcApplication.java
@@ -3,7 +3,9 @@ package com.luisrobbo.cursomc;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
