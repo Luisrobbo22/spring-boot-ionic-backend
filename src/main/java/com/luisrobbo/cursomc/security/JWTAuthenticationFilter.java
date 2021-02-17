e1e0df8ec7a69d848e2cb9e25f9a1998dfd8eb29
luisrobbo <Luisrobbo22>
2021-02-17 13:53:07 -0300

    Expondo o header Authorization

--- src/main/java/com/luisrobbo/cursomc/security/JWTAuthenticationFilter.java
+++ src/main/java/com/luisrobbo/cursomc/security/JWTAuthenticationFilter.java
@@ -58,6 +58,7 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 		String username = ((UserSS) auth.getPrincipal()).getUsername();
 		String token = jwtUtil.generateToken(username);
 		resp.addHeader("Authorization", "Bearer " + token);
+		resp.addHeader("access-control-expose-headers", "Authorization");
 		
 	}
 	
